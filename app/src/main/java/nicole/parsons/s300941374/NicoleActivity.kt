9cfeccb618be9e3a111f104250a5c0db65d817ce
Nicole Parsons <nicoleparsons2930@gmail.com>
2020-10-31 08:44:09 -0400

    Initial

---
+++ app/src/main/java/nicole/parsons/s300941374/NicoleActivity.kt
@@ -0,0 +1,11 @@
+package nicole.parsons.s300941374
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NicoleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
